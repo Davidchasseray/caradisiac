16e485cca18d65a6865a4fb7bd22273bb5a43341
Droopwin <david.chasseray@devinci.fr>
2018-03-16 19:04:29 +0100

    first try

---
+++ src/app/server/Elastic.js
@@ -0,0 +1,64 @@
+const { getBrands } = require('node-car-api');
+const { getModels } = require('node-car-api');
+var fs = require('fs');
+
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  hosts: ['http://localhost:9200']
+});
+
+client.ping({
+  requestTimeout: 30000,
+}, function (error) {
+  if (error) {
+    console.error('elasticsearch cluster is down!');
+  } else {
+    console.log('Everything is ok');
+  }
+});
+
+
+client.indices.create({
+  index: 'cars'
+}, function (err, resp, status) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("create", resp);
+  }
+});
+
+
+
+async function insertIntoElastic() {
+  const brands = await getBrands();
+  const cars = [];
+  brands.forEach(async brand => {
+    var carsWithBrand = await getModels(brand);
+    carsWithBrand.forEach(car => {
+      cars.push(car);
+    });
+  }, () => { fs.writeFileSync('cars.json', cars); });
+
+}
+/*
+  cars.forEach(car => {
+    client.index({
+      index: 'cars',
+      id: car.uuid,
+      type: 'car',
+      body: {
+        "brand": car.brand,
+        "model": car.model,
+        "volume": car.volume,
+        "name": car.name
+      }
+    }, function (err, resp, status) {
+      console.log(resp);
+    });
+  });
+};
+*/
+
+insertIntoElastic();
+exports.insertElastic = insertIntoElastic;
